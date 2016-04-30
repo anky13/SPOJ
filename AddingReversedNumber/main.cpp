dccab8f5094bf0ea49d389eaaf6216cbc5f89e6a
Maquina <BetaHouse@ankits-macbook-pro.local>
2016-04-30 22:39:49 +0530

    Initial Commit

---
+++ AddingReversedNumber/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AddingReversedNumber
+//
+//  Created by Maquina on 30/04/16.
+//  Copyright © 2016 Maquina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
